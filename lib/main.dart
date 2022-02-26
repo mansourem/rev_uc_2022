1d7fc3f843fe32a4663d316cd618387889abee5d
mansourem <89037252+mansourem@users.noreply.github.com>
2022-02-26 16:44:02 -0500

    buttons and counter

--- lib/main.dart
+++ lib/main.dart
@@ -48,16 +48,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  double _counter = 0;
 
-  void _incrementCounter() {
+
+  void _incrementCounter_25() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter+= .25;
+    });
+  }
+
+  void _incrementCounter_50() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter+= .5;
     });
   }
 
@@ -78,38 +90,61 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+        child: Column(children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Wallet:',
+              textAlign: TextAlign.start,
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
+
             ),
-          ],
+        Container(
+          margin: EdgeInsets.all(25),
+          child: TextButton(
+            child: Text(
+              'Button 1',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            onPressed: () {},
+          ),
         ),
+        Container(
+          margin: EdgeInsets.all(25),
+          child: TextButton(
+            child: Text(
+              'Took Out the Trash!',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            //color: Colors.cyan,
+            //textColor: Colors.black,
+            onPressed: _incrementCounter_50,
+          ),
+        ),
+        ]),
+
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _incrementCounter_25,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+// Column is also a layout widget. It takes a list of children and
+// arranges them vertically. By default, it sizes itself to fit its
+// children horizontally, and tries to be as tall as its parent.
+//
+// Invoke "debug painting" (press "p" in the console, choose the
+// "Toggle Debug Paint" action from the Flutter Inspector in Android
+// Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+// to see the wireframe for each widget.
+//
+// Column has various properties to control how it sizes itself and
+// how it positions its children. Here we use mainAxisAlignment to
+// center the children vertically; the main axis here is the vertical
+// axis because Columns are vertical (the cross axis would be
+// horizontal).
+//mainAxisAlignment: MainAxisAlignment.center,
