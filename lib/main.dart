c0d7a580c76ea46e5bbcf78a46a7cdd66d5b986e
Dhriti <gadada@mail.uc.edu>
2022-02-26 22:24:43 -0500

    info and page update

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,11 @@ void main() {
       '/second': (context) => Savings(),
       '/third': (context) => Earnings(),
       '/fourth': (context) => Spending(),
+      '/fifth':(context) => Info(),
     },
   ));
 
 }
-
-
 class HomeRoute extends StatefulWidget {
   @override
   State<StatefulWidget> createState(){
@@ -29,6 +28,12 @@ class _HomeRouteState extends State<HomeRoute>{
       appBar: AppBar(
         title: const Text("Home"),
         backgroundColor: Colors.blue,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:() {
+          Navigator.pushNamed(context,'/fifth');
+        },
+        child: const Icon(Icons.info_outline),
       ),
       body: Center(
         child: Column(
@@ -68,8 +73,9 @@ class _HomeRouteState extends State<HomeRoute>{
                 },
               ),
             ),
-            Text('Amount Earned: ${globals.wallet}'),
-            Text('Amount Saved: ${globals.savings}')
+            const Text(''),
+            Text('Amount Earned: \$${globals.wallet}', style: const TextStyle(fontSize: 24.0)),
+            Text('Amount Saved: \$${globals.savings}', style: const TextStyle(fontSize: 24.0))
           ],
         ),
       ),
@@ -85,10 +91,19 @@ class Savings extends StatefulWidget{
 }
 
 class _SavingsState extends State<Savings>{
-  save5(value) {
-    setState(() {
-      globals.savings+=value;
-    });
+  void save5(amount) {
+    double amount_diff = globals.wallet - amount;
+    if (amount_diff >= 0)
+    {
+      setState(() {
+        globals.savings+=amount;
+        globals.wallet-=amount;
+      });
+    }
+    else
+    {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Invalid Amount"),));
+    }
   }
   TextEditingController saveAmount = TextEditingController();
   @override
@@ -120,7 +135,8 @@ class _SavingsState extends State<Savings>{
                 labelText: 'Save Amount',
               ),
             ),
-            const Text('Lets Start Saving!',),
+            const Text('',),
+            const Text('Let\'s Start Saving!', style: TextStyle(fontSize: 20.0)),
             SizedBox(
               width: 400.0,
               height: 100.0,
@@ -132,7 +148,7 @@ class _SavingsState extends State<Savings>{
                 }
               ),
             ),
-            Text('Savings: ${globals.savings}')
+            Text('Savings: \$${globals.savings}')
           ],
         ),
       ),
@@ -173,16 +189,34 @@ class _EarningsState extends State<Earnings>{
         child: Column(
           children: <Widget>[
             const Text('',),
-            const Text('Lets Start Saving!',),
+            const Text('Let\'s Earn Some Money!', style: TextStyle(fontSize: 20.0)),
             SizedBox(
               width: 400.0,
               height: 100.0,
               child: ElevatedButton(
-                child: const Text('Take Out The Trash'),
+                child: const Text('\$5    Take Out The Trash'),
                 onPressed: earn5,
               ),
             ),
-            Text('Earned: ${globals.wallet}')
+            const Text('',),
+            SizedBox(
+              width: 400.0,
+              height: 100.0,
+              child: ElevatedButton(
+                child: const Text('\$5    Mop the Floor'),
+                onPressed: earn5,
+              ),
+            ),
+            const Text('',),
+            SizedBox(
+              width: 400.0,
+              height: 100.0,
+              child: ElevatedButton(
+                child: const Text('\$5    Do Something'),
+                onPressed: earn5,
+              ),
+            ),
+            Text('Earned: \$${globals.wallet}')
           ],
         ),
       ),
@@ -197,6 +231,19 @@ class Spending extends StatefulWidget{
   }
 }
 class _SpendingState extends State<Spending>{
+  void spend(amount) {
+    double amount_diff = globals.wallet - amount;
+    if (amount_diff >= 0)
+    {
+      setState(() {
+        globals.wallet-=amount;
+      });
+    }
+    else
+    {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Not Enough Earned"),));
+    }
+  }
   @override
   Widget build (BuildContext context) {
     return Scaffold(
@@ -214,7 +261,67 @@ class _SpendingState extends State<Spending>{
           },
         ),
       ),
-      body: const Text("Rewards!"),
+    body: Center(
+      child: Column(
+        children: <Widget>[
+          const Text('',),
+          const Text('Rewards!', style: TextStyle(fontSize: 20.0)),
+          Text('Earned: \$${globals.wallet}'),
+          const Text('',),
+          const Text('Lets Start Spending!',),
+          SizedBox(
+            width: 400.0,
+            height: 100.0,
+            child: ElevatedButton(
+              child: const Text('TV Time! - \$5'),
+                onPressed: (){
+                  spend(5);
+                },
+            ),
+          ),
+          Text('Earned: ${globals.wallet}')
+        ],
+      ),
+    ),
+    );
+  }
+}
+
+class Info extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState(){
+    return _InfoState();
+  }
+}
+class _InfoState extends State<Info>{
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Info"),
+        automaticallyImplyLeading: false,
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.home),
+              onPressed: () {
+                Navigator.pushNamed(context,'/');
+              },
+            );
+          },
+        ),
+      ),
+      body: Center(
+
+        child: Column(
+          children: <Widget>[
+            const Text('',),
+            const Text('Rewards!', style: TextStyle(fontSize: 20.0)),
+
+            Text('Earned: \$${globals.wallet}')
+          ],
+        ),
+      ),
     );
   }
 }
