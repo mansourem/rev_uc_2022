73b44291b267167491fd0c8231dc1cf4bcdfe82e
Pranitha Jemmalla <pranitha.jemmallad2@gmail.com>
2022-02-26 21:59:15 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,233 @@
 import 'package:flutter/material.dart';
+import 'global_wallet.dart' as globals;
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: '/',
+    routes:{
+      '/': (context) => HomeRoute(),
+      '/second': (context) => Savings(),
+      '/third': (context) => Earnings(),
+      '/fourth': (context) => Spending(),
+    },
+  ));
+
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+class HomeRoute extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+  State<StatefulWidget> createState(){
+    return _HomeRouteState();
+  }
+}
+class _HomeRouteState extends State<HomeRoute>{
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        backgroundColor: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const Text('',),
+            SizedBox(
+              width: 400.0,
+              height: 200.0,
+              child: ElevatedButton(
+                child: const Text('Savings'),
+                onPressed: () {
+                  Navigator.pushNamed(context,'/second');
+                },
+              ),
+            ),
+            const Text('',),
+
+            SizedBox(
+              width: 400.0,
+              height: 200.0,
+              child: ElevatedButton(
+                child: const Text('Earnings'),
+                onPressed: () {
+                  Navigator.pushNamed(context,'/third');
+                },
+              ),
+            ),
+            const Text('',),
+
+            SizedBox(
+              width: 400.0,
+              height: 200.0,
+              child: ElevatedButton(
+                child: const Text('Spending'),
+                onPressed: () {
+                  Navigator.pushNamed(context,'/fourth');
+                },
+              ),
+            ),
+            Text('Amount Earned: ${globals.wallet}'),
+            Text('Amount Saved: ${globals.savings}')
+          ],
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Savings extends StatefulWidget{
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState(){
+    return _SavingsState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _SavingsState extends State<Savings>{
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build (BuildContext context){
+    void save5(amount) {
+      double amount_diff = globals.wallet - amount;
+      if (amount_diff > 0)
+      {
+        setState(() {
+          globals.savings+=amount;
+          globals.wallet-=amount;
+        });
+      }
+      else
+      {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Invalid Amount"),
+        ));
+      }
+
+    }
+    TextEditingController saveAmount = TextEditingController();
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Savings"),
+        automaticallyImplyLeading: false,
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.home),
+              onPressed: () {
+                Navigator.pushNamed(context,'/');
+              },
+            );
+          },
+        ),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const Text('',),
+            TextField(
+              keyboardType: TextInputType.number,
+              controller: saveAmount,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Save Amount',
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const Text('Lets Start Saving!',),
+            SizedBox(
+              width: 400.0,
+              height: 100.0,
+              child: ElevatedButton(
+                child: const Text('Save'),
+                  onPressed: () {
+                    double i = double.parse(saveAmount.text);
+                    save5(i);
+                  }
+              ),
             ),
+            Text('Savings: ${globals.savings}'),
+            Text('Wallet: ${globals.wallet}')
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+class Earnings extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState(){
+    return _EarningsState();
+  }
+}
+
+class _EarningsState extends State<Earnings>{
+  void earn5() {
+    setState(() {
+      globals.wallet+=5;
+    });
+  }
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Earnings"),
+        automaticallyImplyLeading: false,
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.home),
+              onPressed: () {
+                Navigator.pushNamed(context,'/');
+              },
+            );
+          },
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const Text('',),
+            const Text('Lets Start Saving!',),
+            SizedBox(
+              width: 400.0,
+              height: 100.0,
+              child: ElevatedButton(
+                child: const Text('Take Out The Trash'),
+                onPressed: earn5,
+              ),
+            ),
+            Text('Earned: ${globals.wallet}')
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Spending extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState(){
+    return _SpendingState();
+  }
+}
+class _SpendingState extends State<Spending>{
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Spending"),
+        automaticallyImplyLeading: false,
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.home),
+              onPressed: () {
+                Navigator.pushNamed(context,'/');
+              },
+            );
+          },
+        ),
+      ),
+      body: const Text("Rewards!"),
     );
   }
 }
